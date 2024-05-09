8df426bab9dd74bc67605eb9e5a4fdd967727e45
chiedozieu <chiedozieugwor@gmail.com>
2024-05-09 09:57:12 +0100

    redox tool kit (store, slice, useDispatch etc)

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js';
+import { useDispatch, useSelector } from 'react-redux';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError ] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value})
@@ -15,34 +17,30 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
-      const res = await fetch('/api/auth/signin ', {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
-      const data = await res.json()
-      setLoading(false);
-     
+      const data = await res.json() 
       
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data));
         return 
+        
       }
+      dispatch(signInSuccess(data));
       navigate('/');
       
     } catch (error) {
-      setLoading(false);
-      setError(true)      
+      dispatch(signInFailure(error));      
     }  
   }
  
-   
-
-
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-center text-3xl font-semibold my-7">Sign In</h1>
@@ -60,10 +58,12 @@ const SignIn = () => {
         </Link>
       </div>
       
-          <p className='text-red-700 mt-5'>{error && 'Something went wrong'}</p>
+          <p className='text-red-700 mt-5'>{error? error.message || 'Something went wrong' : ''}</p>
      
     </div>
   )
 }
 
 export default SignIn
+
+
